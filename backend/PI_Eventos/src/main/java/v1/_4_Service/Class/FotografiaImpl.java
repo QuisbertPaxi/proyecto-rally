acc6fe1a902f00d733dd1a7488f76a9539e2bb69
BrendaQuisbertPaxi <bquipax803@g.educaand.es>
2025-06-17 05:23:01 +0200

    actualizado devido a equivocarme en el repositorio para el deploy

--- backend/PI_Eventos/src/main/java/v1/_4_Service/Class/FotografiaImpl.java
+++ backend/PI_Eventos/src/main/java/v1/_4_Service/Class/FotografiaImpl.java
@@ -14,6 +14,7 @@ import v1._2_DTO.FotografiaDTO;
 import v1._3_Repository.ConcursoRepository;
 import v1._3_Repository.FotografiaRepository;
 import v1._3_Repository.UsuarioRepository;
+import v1._3_Repository.VotoRepository;
 import v1._4_Service.Interface.FotografiaService;
 
 import java.net.URI;
@@ -39,6 +40,9 @@ public class FotografiaImpl implements FotografiaService {
 
     @Autowired
     ConcursoRepository concursoRepository;
+
+    @Autowired
+    VotoRepository votosRepository;
 
     String roleParticipante = "participante";
     String roleAdmin = "admin";
@@ -185,6 +189,12 @@ public class FotografiaImpl implements FotografiaService {
        // fotografia.setFechaModificacion(new Timestamp(new Date().getTime()));
        // fotografiaRepository.save(fotografia);
 
+        long votoscount = votosRepository.contarVotosPorFotografia(fotografia.getId());
+
+        if (votoscount > 0){
+            votosRepository.eliminarVotosPorFotografia(fotografia.getId());
+        }
+
         fotografiaRepository.eliminarFotografiaPorId(fotografiaDTO.getId());
 
         return ResponseEntity.ok("Se ha eliminado con exito la fotografia");
@@ -248,11 +258,24 @@ public class FotografiaImpl implements FotografiaService {
             return  ResponseEntity.notFound().build();
         }
 
-        if(aprobado){
-            fotografia.setEstado("APROBADO");
-        }else{
-            fotografia.setEstado("RECHAZADO");
+//        if(aprobado){
+//            fotografia.setEstado("APROBADO");
+//        }else{
+//            fotografia.setEstado("RECHAZADO");
+//        }
+
+        if (!aprobado) {
+            long votoscount = votosRepository.contarVotosPorFotografia(fotografia.getId());
+
+            if (votoscount > 0){
+                votosRepository.eliminarVotosPorFotografia(fotografia.getId());
+            }
+
+            fotografiaRepository.eliminarFotografiaPorId(fotografia.getId());
+            return ResponseEntity.noContent().build();
         }
+
+        fotografia.setEstado("APROBADO");
         fotografia.setUsuarioAprobacion(user.getUserName());
         fotografia.setFechaAprobacion(new Timestamp(new Date().getTime()));
 
